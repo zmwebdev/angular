0c31a4660d1fff83b911f37b1ed166de9501ee37
zmwebdev@gmail.com <zmwebdev@gmail.com>
2015-05-21 22:41:53 +0200

    m

---
+++ 02-Directives-Controllers/02-creating-controller.js
@@ -0,0 +1,6 @@
+  angular.module('notesApp', [])
+    .controller('MainCtrl', [function() {
+       this.helloMsg = 'Hello ';
+       // var goodbyeMsg = 'Goodbye ';
+       this.goodbyeMsg = 'Goodbye ';
+  }]);
